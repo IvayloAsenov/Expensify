356c7a90244817995d198faca86334b92d227ea8
IvayloAsenov <ivaylo.asenov@hotmail.com>
2019-01-04 17:41:08 -0500

    added routed

--- src/app.js
+++ src/app.js
@@ -1,6 +1,49 @@
 import React     from 'react';
 import ReactDOM  from 'react-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>This is my boiler plate</p>, document.getElementById('app'));
+const ExpenseDashBoardPage = () => (
+    <div>
+        This is from my dashboard component
+    </div>
+);
+
+const AddExpensePage = () => (
+    <div>
+        Add Expense page!
+    </div>
+);
+
+const EditExpensePage = () => (
+    <div>
+        Edit page
+    </div>
+);
+
+const HelpPage = () => (
+    <div>
+        Help page
+    </div>
+);
+
+const NotFoundPage = () => (
+    <div>
+        404!
+    </div>
+);
+
+const routes = (
+    <BrowserRouter>
+        <Switch>
+            <Route path = '/' component = {ExpenseDashBoardPage} exact = {true}/>
+            <Route path = '/create' component = {AddExpensePage} exact = {true}/>
+            <Route path = '/edit' component = {EditExpensePage} exact = {true}/>
+            <Route path = '/help' component = {HelpPage} exact = {true}/>
+            <Route component = {NotFoundPage}/>
+        </Switch> 
+    </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
