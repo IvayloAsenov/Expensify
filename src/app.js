b8f861fe9831b60edd52e16b2777581497fbf145
IvayloAsenov <ivaylo.asenov@hotmail.com>
2019-01-06 20:20:40 -0500

    divided in components and dynamic url for edit expense

--- src/app.js
+++ src/app.js
@@ -1,62 +1,8 @@
 import React     from 'react';
 import ReactDOM  from 'react-dom';
-import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
+import AppRouter from './routers/AppRouter';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-const ExpenseDashBoardPage = () => (
-    <div>
-        This is from my dashboard component
-    </div>
-);
 
-const AddExpensePage = () => (
-    <div>
-        Add Expense page!
-    </div>
-);
-
-const EditExpensePage = () => (
-    <div>
-        Edit page
-    </div>
-);
-
-const HelpPage = () => (
-    <div>
-        Help page
-    </div>
-);
-
-const NotFoundPage = () => (
-    <div>
-        404 - <Link to = '/'>Go back to home page!</Link>
-    </div>
-);
-
-const Header = () => (
-    <header>
-        <h1>Expensify</h1>
-        <NavLink to = '/' activeClassName = 'is-active' exact = {true}>Dashboard</NavLink>
-        <NavLink to = '/create' activeClassName = 'is-active' exact = {true}>Create Expense</NavLink>
-        <NavLink to = '/edit' activeClassName = 'is-active' exact = {true}>Edit Expense</NavLink>
-        <NavLink to = '/help' activeClassName = 'is-active' exact = {true}>Help</NavLink>        
-    </header>
-);
-
-const routes = (
-    <BrowserRouter>
-        <div>
-            <Header/>
-            <Switch>
-                <Route path = '/' component = {ExpenseDashBoardPage} exact = {true}/>
-                <Route path = '/create' component = {AddExpensePage} exact = {true}/>
-                <Route path = '/edit' component = {EditExpensePage} exact = {true}/>
-                <Route path = '/help' component = {HelpPage} exact = {true}/>
-                <Route component = {NotFoundPage}/>
-            </Switch> 
-        </div>
-    </BrowserRouter>
-);
-
-ReactDOM.render(routes, document.getElementById('app'));
+ReactDOM.render(<AppRouter />, document.getElementById('app'));
