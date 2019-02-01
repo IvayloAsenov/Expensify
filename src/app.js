e5782d3b044d69367a114617e2f2a74d6b9bc1ea
IvayloAsenov <ivaylo.asenov@hotmail.com>
2019-02-01 09:10:30 -0500

    created expense form page with basic logic

--- src/app.js
+++ src/app.js
@@ -12,30 +12,33 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-const waterBill = {
-    description: 'water bill', 
-    note: 'This is for the water',
-    amount: 4500
-};
-
 const gasBill = {
     description: 'gas bill', 
     note: 'This is for the gas',
-    amount: 100
+    amount: 100,
+    createdAt: 5000
+};
+
+const waterBill = {
+    description: 'water bill', 
+    note: 'This is for the water',
+    amount: 4500, 
+    createdAt: 100
+};
+
+const rent = {
+    description: 'rent', 
+    note: 'This is for the water',
+    amount: 15500, 
+    createdAt: 500
 };
 
 store.dispatch(addExpense(waterBill));
 store.dispatch(addExpense(gasBill));
-store.dispatch(setTextFilter('water'));
-
-setTimeout(() => {
-    store.dispatch(setTextFilter('bill'));
-}, 3000);
+store.dispatch(addExpense(rent));
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-
-console.log(visibleExpenses);
 
 const jsx = (
     <Provider store={store}>
