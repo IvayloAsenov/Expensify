534254f83981febbe9b58f361a01ef0fe5e9e945
IvayloAsenov <ivaylo.asenov@hotmail.com>
2019-04-30 14:16:16 -0400

    set up private route to block no logged in users from accessing those pages

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -7,20 +7,19 @@ import AddExpensePage from '../components/AddExpensePage.js';
 import EditExpensePage from '../components/EditExpensePage.js';
 import HelpPage from '../components/HelpPage.js';
 import NotFoundPage from '../components/NotFoundPage.js';
-import Header from '../components/Header.js';
 import Login from '../components/Login.js';
+import PrivateRoute from './PrivateRoute.js';
 
 export const history = createHistory();
 
 const AppRouter = () => (
     <Router history={history}>
         <div>
-            <Header/>
             <Switch>
                 <Route path = '/' component = {Login} exact = {true}/>
-                <Route path = '/dashboard' component = {ExpenseDashBoardPage} exact = {true}/>
-                <Route path = '/create' component = {AddExpensePage} exact = {true}/>
-                <Route path = '/edit/:id' component = {EditExpensePage} exact = {true}/>
+                <PrivateRoute path = '/dashboard' component = {ExpenseDashBoardPage} exact = {true}/>
+                <PrivateRoute path = '/create' component = {AddExpensePage} exact = {true}/>
+                <PrivateRoute path = '/edit/:id' component = {EditExpensePage} exact = {true}/>
                 <Route path = '/help' component = {HelpPage} exact = {true}/>
                 <Route component = {NotFoundPage}/>
             </Switch> 
